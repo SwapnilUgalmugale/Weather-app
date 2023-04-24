8395efdfcf21b955d670c432017b5ecddb6a53d3
D8_63020_Swapnil_Ugalmugale <swapnilugalmugale2002@gmail.com>
2023-04-24 20:54:24 +0530

    added search bar with geolocationapi

---
+++ src/api.js
@@ -0,0 +1,12 @@
+
+export const geoAPIOptions = {
+	method: 'GET',
+	headers: {
+		'content-type': 'application/octet-stream',
+		'X-RapidAPI-Key': '3d6f07d827msh0e53238dafb6cd1p122ef3jsn692825a731ae',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+    }
+};
+
+export const geoAPIurl = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+
--- src/components/search/search.js
+++ src/components/search/search.js
@@ -1,6 +1,46 @@
+import { useState } from "react";
+import { AsyncPaginate } from "react-select-async-paginate";
+import {geoAPIurl, geoAPIOptions} from '../../api';
 
-const Search = () =>{
-    return ('Hello')
+const Search = ({onSearchChange}) =>{
+    
+    const [search, setSearch] = useState(null);
+
+    const loadOptions = (inputValue) => {
+        return  fetch(
+            `${geoAPIurl}/cities?namePrefix=${inputValue}`, geoAPIOptions
+            )
+        .then((response) => response.json())
+        .then((response) => {
+            return {
+                options: response.data.map((city) => {
+                    return {
+                        value: `${city.latitude} ${city.longitude}`,
+                        label: `${city.name}, ${city.countryCode}`,
+                        
+                    }
+                })
+            }
+        })
+        .catch((err) => console.error(err));
+            
+        
+    };
+    
+    const handleOnChange = (searchData) =>{
+        setSearch(searchData);
+        onSearchChange(searchData);
+    }
+
+    return (
+        <AsyncPaginate
+          placeholder="Search for city"
+          debounceTimeout={600}
+          value={search}
+          onChange={handleOnChange}
+          loadOptions={loadOptions}
+        />
+    )
 }
 
 export default Search;
