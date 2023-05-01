d6dc320c7bfce7a17f85990dd0a7a3cbb6d55364
D8_63020_Swapnil_Ugalmugale <swapnilugalmugale2002@gmail.com>
2023-05-02 01:05:03 +0530

    changes in modified code for getting current data

--- src/App.js
+++ src/App.js
@@ -8,35 +8,41 @@ import { useState } from 'react';
 
 
 function App() {
+
   const [ currentWeather, setCurrentWeather ] = useState(null);
   const [ forecast, setForecast ] = useState(null);
+  
 
   const handleOnSearchChange = (searchData) => {
    const [ lat, lon ] = searchData.value.split(" ");
 
-   const currentWeatherFetch = fetch(`${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid= ${WEATHER_API_KEY}`);
+   const currentWeatherFetch = fetch(`${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`);
 
-   const forcastFetch = fetch(`${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid= ${WEATHER_API_KEY}`);
-   
+   const forecastFetch = fetch(`${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`);
 
-   Promise.all([currentWeatherFetch, forcastFetch])
-   .then(async (response) => {
-    const weatherResponse = await response[0].json();
-    const forcastResponse = await response[1].json();
+   Promise.all([ currentWeatherFetch, forecastFetch ])
+    .then(async (response) => {
+      const weatherResponse = await response[0].json();
+      const forecastResponse = await response[1].json();
 
-    setCurrentWeather({ city: searchData.label,...weatherResponse });
-    setForecast({ city: searchData.label,...forcastResponse });
-   });
+
+      setCurrentWeather({city:searchData.label, weatherResponse});
+      setForecast({city: searchData.label, forecastResponse});
+
+    })
+    .catch((err) => console.log((err)));
 
   }
-  
+
   console.log(currentWeather);
   console.log(forecast);
+
+  
 
   return (
     <div className="container">
     <Search onSearchChange={handleOnSearchChange} />
-    <CurrentWeather />
+    { currentWeather && <CurrentWeather data={currentWeather}  /> }
     </div>
   );
 }
--- src/components/current-weather/current-weather.js
+++ src/components/current-weather/current-weather.js
@@ -2,37 +2,37 @@ import "./current-weather.css"
 //import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 
-const CurrentWeather = () => {
+const CurrentWeather = ({data}) => {
     return (
         <div className="weather">
             <div className="top">
                 <div>
-                   <p className="city">Pune</p>
-                   <p className="weather-description">Sunny</p>
+                   <p className="city">{data.city}</p>
+                   <p className="weather-description">{data.weatherResponse.weather[0].description}</p>
                 </div>
-                <img alt="weather" className="weather-icon" src="icons/sun.png" />
+                <img alt="weather" className="weather-icon" src={`icons/${data.weatherResponse.weather[0].icon}.png`} />
             </div>
             <div className="bottom">
-                <p className="temperature">18°C</p>
+                <p className="temperature">{Math.round(data.weatherResponse.main.temp)}°C</p>
                 <div className="details">
                     <div className="parameter-row">
                         <span className="parameter-lable">Details</span>
                     </div>
                     <div className="parameter-row">
-                        <span className="parameter-lable">Feels Like</span>
-                        <span className="parameter-value">22</span>
+                        <span className="parameter-lable">Feels like</span>
+                        <span className="parameter-value">{Math.round(data.weatherResponse.main.feels_like)}°C</span>
                     </div>
                     <div className="parameter-row">
                         <span className="parameter-lable">Wind speed</span>
-                        <span className="parameter-value">8.8km/h</span>
+                        <span className="parameter-value">{data.weatherResponse.wind.speed}km/h</span>
                     </div>
                     <div className="parameter-row">
                         <span className="parameter-lable">Humidity</span>
-                        <span className="parameter-value">50%</span>
+                        <span className="parameter-value">{data.weatherResponse.main.humidity}%</span>
                     </div>
                     <div className="parameter-row">
                         <span className="parameter-lable">Pressure</span>
-                        <span className="parameter-value">1011mbar</span>
+                        <span className="parameter-value">{data.weatherResponse.main.pressure}mbar</span>
                     </div>
                 </div>
             </div>
