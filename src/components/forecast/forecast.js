3feaae264dd3dbb72b1971638bbe9d87e24b1ab8
D8_63020_Swapnil_Ugalmugale <swapnilugalmugale2002@gmail.com>
2023-05-02 14:56:20 +0530

    created week days component for getting day wise info

--- src/components/forecast/forecast.js
+++ src/components/forecast/forecast.js
@@ -1,35 +1,56 @@
-import { Accordion, 
-    AccordionItem, 
-    AccordionItemHeading, 
-    AccordionItemPanel,
-    AccordionItemButton
+import {
+  Accordion,
+  AccordionItem,
+  AccordionItemHeading,
+  AccordionItemPanel,
+  AccordionItemButton,
 } from "react-accessible-accordion";
 
+const Week_Days = [
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+  "Sunday",
+];
 
+const Forecast = ({ data }) => {
+  const dayInAWeek = new Date().getDate();
+  const forecastDays = Week_Days.slice(dayInAWeek, Week_Days.length).concat(
+    Week_Days.slice(0, dayInAWeek)
+  );
 
-const Forecast = ({data}) => {
-    return (
+  //check whether data is getting or not on console 
+  //console.log(forecastDays);
+
+  return (
     <>
-        <label className="title">Daily</label>
-        <Accordion allowZeroExpanded>
-            {data.forecastResponse.list.slice(0,7).map((item,idx) => (
-                <AccordionItem key={idx}>
-                    <AccordionItemHeading>
-                        <AccordionItemButton>
-                            <div className="daily-item">
-                                <img alt="weather" className="icon-small" src={`icons/${item.weather[0].icon}.png`}/>
-                            </div>
-                        </AccordionItemButton>
-                    </AccordionItemHeading>
-                    <AccordionItemPanel></AccordionItemPanel>
-                </AccordionItem>
-            
-            ))}
-         
+      <label className="title">Daily</label>
+      <Accordion allowZeroExpanded>
+        {data.forecastResponse.list.slice(0, 7).map((item, idx) => (
+          <AccordionItem key={idx}>
+            <AccordionItemHeading>
+              <AccordionItemButton>
+                <div className="daily-item">
+                  <img
+                    alt="weather"
+                    className="icon-small"
+                    src={`icons/${item.weather[0].icon}.png`}
+                  />
+                  <label className="day">{forecastDays[idx]}</label>
+                  <label className="description"> { item.weather[0].description }</label>
 
-        </Accordion>
+                </div>
+              </AccordionItemButton>
+            </AccordionItemHeading>
+            <AccordionItemPanel></AccordionItemPanel>
+          </AccordionItem>
+        ))}
+      </Accordion>
     </>
-    )
-}
+  );
+};
 
 export default Forecast;
